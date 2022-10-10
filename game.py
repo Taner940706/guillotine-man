6d6406db67bfc2d4eeee6d053826fabbfbb522c9
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2022-10-10 08:08:30 -0700

    game logic 2

--- game.py
+++ game.py
@@ -19,11 +19,10 @@ class Game(tk.Toplevel):
         super().__init__()
         self.title('New Game')
         self.geometry('700x500')
-       # self.resizable(False, False)
+        self.resizable(False, False)
         self.iconbitmap('./images/icon.ico')
         self.lost_game = 0
 
-        self.A = tk.IntVar()
         self.frame_draw = Frame(self, width=200, height=200)
         self.frame_draw.pack()
         self.frame_draw.place(anchor='center', relx=0.5, rely=0.5)
@@ -35,8 +34,8 @@ class Game(tk.Toplevel):
         self.label.pack()
         self.score_label = Label(self, text="SCORE")
         self.score_label.place(relx=5.0, rely=0.0, anchor='ne')
-        self.score_text = Label(self, textvariable=self.A)
-        self.score_label.place(relx=1.0, rely=0.0, anchor='ne')
+        self.score_text = Label(self, text=10)
+        self.score_text.place(relx=1.0, rely=0.0, anchor='ne')
 
         self.question_frame = LabelFrame(self, text="Question number", padx=30, pady=20)
         self.category = Label(self, text="Category...")
@@ -57,44 +56,59 @@ class Game(tk.Toplevel):
         self.question.config(text=questions[self.player_question][1])
 
     def play_game(self):
+        score = int(self.score_text['text'])
         ans = tkinter.StringVar(name='ans', value=questions[self.player_question][2])
-        if self.answer.get(1.0, "end-1c") == ans.get():
-            del questions[self.player_question]
-            messagebox.showinfo("showinfo", "Your answer is correct! Well done!")
-            self.player_question = random.choice(list(questions.keys()))
-            self.category.config(text=questions[self.player_question][0])
-            self.question.config(text=questions[self.player_question][1])
-            self.score_text += 10
-
+        if len(questions) == 1:
+            self.score_text.config(text=score + 10)
+            messagebox.showinfo("showinfo", "You Win!")
+            player_name = simpledialog.askstring("Input", "Type your name:")
+            ranking.update({player_name: self.score_text['text']})
+            file = open('rating.json', 'w')
+            json.dump(ranking, file, indent=4)
+            file.close()
+            Game.quit(self)
+            return
         else:
-            messagebox.showerror("showerror", "Not correct!")
-            if self.lost_game == 0:
-                self.img = ImageTk.PhotoImage(Image.open("./images/only_guillotine.png"))
-                # Create a Label Widget to display the text or Image
-                self.label = Label(self.frame_draw, image=self.img)
-                self.label.pack()
-                self.lost_game += 1
-            elif self.lost_game == 1:
-                self.img = ImageTk.PhotoImage(Image.open("./images/with_prisoner.png"))
-                # Create a Label Widget to display the text or Image
-                self.label = Label(self.frame_draw, image=self.img)
-                self.label.pack()
-                self.lost_game += 1
-            elif self.lost_game == 2:
-                self.img = ImageTk.PhotoImage(Image.open("./images/with_prisoner_judge.png"))
-                # Create a Label Widget to display the text or Image
-                self.label = Label(self.frame_draw, image=self.img)
-                self.label.pack()
-                self.lost_game += 1
-            elif self.lost_game == 3:
-                self.img = ImageTk.PhotoImage(Image.open("./images/game_over.png"))
-                # Create a Label Widget to display the text or Image
-                self.label = Label(self.frame_draw, image=self.img)
-                self.label.pack()
-                messagebox.showinfo("showinfo", "Game Over!")
-                player_name = simpledialog.askstring("Input", "Type your name:")
-                ranking[player_name] = self.score_text
-                Game.quit(self)
+            if self.answer.get(1.0, "end-1c") == ans.get():
+                del questions[self.player_question]
+                messagebox.showinfo("showinfo", "Your answer is correct! Well done!")
+                self.player_question = random.choice(list(questions.keys()))
+                self.category.config(text=questions[self.player_question][0])
+                self.question.config(text=questions[self.player_question][1])
+                self.score_text.config(text=score+10)
+
+            else:
+                messagebox.showerror("showerror", "Not correct!")
+                if self.lost_game == 0:
+                    self.img = ImageTk.PhotoImage(Image.open("./images/only_guillotine.png"))
+                    # Create a Label Widget to display the text or Image
+                    self.label = Label(self.frame_draw, image=self.img)
+                    self.label.pack()
+                    self.lost_game += 1
+                elif self.lost_game == 1:
+                    self.img = ImageTk.PhotoImage(Image.open("./images/with_prisoner.png"))
+                    # Create a Label Widget to display the text or Image
+                    self.label = Label(self.frame_draw, image=self.img)
+                    self.label.pack()
+                    self.lost_game += 1
+                elif self.lost_game == 2:
+                    self.img = ImageTk.PhotoImage(Image.open("./images/with_prisoner_judge.png"))
+                    # Create a Label Widget to display the text or Image
+                    self.label = Label(self.frame_draw, image=self.img)
+                    self.label.pack()
+                    self.lost_game += 1
+                elif self.lost_game == 3:
+                    self.img = ImageTk.PhotoImage(Image.open("./images/game_over.png"))
+                    # Create a Label Widget to display the text or Image
+                    self.label = Label(self.frame_draw, image=self.img)
+                    self.label.pack()
+                    messagebox.showinfo("showinfo", "Game Over!")
+                    player_name = simpledialog.askstring("Input", "Type your name:")
+                    ranking.update({player_name: self.score_text['text']})
+                    file = open('rating.json', 'w')
+                    json.dump(ranking, file, indent=4)
+                    file.close()
+                    Game.quit(self)
 
 
 if __name__ == "__main__":
